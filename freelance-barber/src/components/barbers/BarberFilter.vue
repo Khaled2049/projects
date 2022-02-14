3b4d8c43749a42a029808c6e9ade36c05bb1dafc
Khaled Hossain <khaledhossain.not@gmail.com>
2022-02-14 08:40:51 -0600

    Added quicksort

--- freelance-barber/src/components/barbers/BarberFilter.vue
+++ freelance-barber/src/components/barbers/BarberFilter.vue
@@ -16,7 +16,14 @@
     <span class="filter-option">
         <input type="checkbox" id="West" checked @change="setFilter"/>
         <label for="West">West</label>
-    </span>
+    </span>    
+    </base-card>
+    <base-card>
+        <h1>Barbers near you!</h1>
+        <button @click="sortBarbers">Sort By Nearest</button>
+        <div v-for="(barber, idx) in barbers" :key="idx">
+            <h2>{{ barber.name }} is {{ barber.distance }} miles away from you.</h2>
+        </div>
     </base-card>
 </template>
 
@@ -30,7 +37,23 @@ export default {
                 South: true, 
                 East: true, 
                 West: true,
+            },
+            barbers: [
+                {'distance': 2, name: 'John'},
+                {'distance': 4, name: 'Sarah'},
+                {'distance': 7, name: 'Bill'},
+                {'distance': 1, name: 'Joe'},
+                {'distance': 8, name: 'Adriana'},
+            ],
+        }
+    },
+    computed: {
+        getDistances() {
+            let barberDistances = [];
+            for (let barber in this.barbers) {
+                barberDistances.push(this.barbers[barber].distance);
             }
+            return barberDistances;
         }
     },
     methods: {
@@ -45,6 +68,40 @@ export default {
             console.log(updatedFilters);
             this.filters = updatedFilters;
             this.$emit('change-filter', updatedFilters);
+        },
+        sortBarbers() {
+            let barbers = this.getDistances;
+            let sortedBarbersDs = this.quickSort(barbers);
+            for (let d in sortedBarbersDs) {
+                for (let i=0;i<sortedBarbersDs; i++) {
+                    if(this.barbers[d].distance == sortedBarbersDs[i]) {
+                        
+                    } 
+                }
+            }
+        },
+        quickSort(array) {
+            if (array.length === 1) {
+                return array;
+            } 
+            const pivot = array[array.length - 1];
+            const leftArr = [];
+            const rightArr = [];
+            for (let i=0; i < array.length - 1; i++) {
+                if (array[i] < pivot) {
+                    leftArr.push(array[i]);
+                } else {
+                    rightArr.push(array[i]);
+                }
+            }
+
+            if (leftArr.length > 0 && rightArr > 0) {
+                return [...this.quickSort(leftArr), pivot, ...this.quickSort(rightArr)];
+            } else if(leftArr.length > 0) {
+                return [...this.quickSort(leftArr), pivot];
+            } else {
+                return [pivot, ...this.quickSort(rightArr)];
+            }
         }
     }
 }
