0534bcd9e7d3a9048c31563037bfcfcc833ad86a
Khaled Hossain <khaledhossain.not@gmail.com>
2022-04-24 23:30:15 -0500

    added final project

---
+++ photo-sorter/frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>  
+    <photo-sorter />
+  </div>
+</template>
+
+<script>
+import photoSorter from './components/PhotoSorter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    photoSorter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
