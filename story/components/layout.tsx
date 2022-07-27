166dd17a552ed7487da69e066e82a08e6fbaf9b7
Khaled Hossain <khaledhossain.not@gmail.com>
2022-07-26 22:19:53 -0500

    Added Nav

---
+++ story/components/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+interface ILayoutProps {
+  children: any;
+}
+
+export default function Layout({ children }: ILayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      {/* <Footer /> */}
+    </>
+  );
+}
--- story/pages/posts/[slug].tsx
+++ story/pages/posts/[slug].tsx
@@ -24,6 +24,19 @@ const PostDetails = ({ post }: IPostDetailsProps) => {
 
 export const getStaticProps = async ({ params }: any) => {
   const data = await getPostDetails(params.slug);
+  // const data = [
+  //   {
+  //     id: 'cl5zeifh2593b0dk1i23f3xci',
+  //     author: { name: 'Khaled Hossain', id: 'cl5zeg848e7aa0bj0gfqas2zl' },
+  //     content: {
+  //       text: "Lorem Ipsum\\n is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  //     },
+  //     coverImage: null,
+  //     title: 'Test Blog',
+  //     slug: 'post-1',
+  //     excerpt: 'This is a test.',
+  //   },
+  // ];
   return {
     props: {
       post: data,
