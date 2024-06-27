29ed02b3cead9b444410a66bfa937392238ab4a1
Khaled Hossain <khaledhossain.not@gmail.com>
2024-06-27 18:08:10 -0500

    Cleanup

--- colonoready/src/components/scheduler.tsx
+++ colonoready/src/components/scheduler.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useNavigate } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
 
 const Scheduler = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
+  const { control, handleSubmit } = useForm();
   const [checked, setChecked] = useState(true);
   const navigate = useNavigate();
 
@@ -12,45 +13,139 @@ const Scheduler = () => {
     setChecked(!checked);
   };
 
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <div className="max-w-md mx-auto my-8 p-6 bg-white rounded-lg">
-      <h2 className="text-2xl font-semibold mb-4">Schedule Appointment</h2>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-md mx-auto my-8 p-6 bg-white rounded-lg"
+    >
       <div className="mb-4">
         <label htmlFor="date" className="block text-gray-700 font-medium mb-2">
           Select Date
         </label>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date: any) => setSelectedDate(date)}
-          placeholderText="MM/DD/YYYY"
-          dateFormat="MM/dd/yyyy"
-          id="date"
-          className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        <Controller
+          control={control}
+          name="date"
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+              placeholderText="MM/DD/YYYY"
+              dateFormat="MM/dd/yyyy"
+              id="date"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          )}
         />
       </div>
       <div className="mb-4">
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={toggleChecked}
-          id="emailReminders"
-          className="mr-2 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-        />
-        <label htmlFor="emailReminders" className="text-gray-700">
-          Email Reminders
+        <label htmlFor="time" className="block text-gray-700 font-medium mb-2">
+          Select Time
         </label>
+        <Controller
+          control={control}
+          name="time"
+          render={({ field }) => (
+            <DatePicker
+              {...field}
+              selected={field.value}
+              onChange={(time) => field.onChange(time)}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="Time"
+              dateFormat="h:mm aa"
+              placeholderText="hh:mm aa"
+              id="time"
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+          )}
+        />
       </div>
+      <div className="mb-4">
+        <label className="block text-gray-700 font-medium mb-2">
+          Select Option
+        </label>
+        <div className="flex items-center mb-2">
+          <Controller
+            control={control}
+            name="option"
+            defaultValue="Trilyte"
+            render={({ field }) => (
+              <>
+                <input
+                  {...field}
+                  type="radio"
+                  id="trilyte"
+                  value="Trilyte"
+                  checked={field.value === "Trilyte"}
+                  onChange={() => field.onChange("Trilyte")}
+                  className="mr-2"
+                />
+                <label htmlFor="trilyte" className="text-gray-700">
+                  Trilyte
+                </label>
+              </>
+            )}
+          />
+        </div>
+        <div className="flex items-center">
+          <Controller
+            control={control}
+            name="option"
+            render={({ field }) => (
+              <>
+                <input
+                  {...field}
+                  type="radio"
+                  id="gatoradeMiralax"
+                  value="Gatorade/Miralax"
+                  checked={field.value === "Gatorade/Miralax"}
+                  onChange={() => field.onChange("Gatorade/Miralax")}
+                  className="mr-2"
+                />
+                <label htmlFor="gatoradeMiralax" className="text-gray-700">
+                  Gatorade/Miralax
+                </label>
+              </>
+            )}
+          />
+        </div>
+      </div>
+      {/* <div className="mb-4">
+        <Controller
+          control={control}
+          name="emailReminders"
+          defaultValue={true}
+          render={({ field }) => (
+            <div className="flex items-center">
+              <input
+                {...field}
+                type="checkbox"
+                checked={field.value}
+                onChange={(e) => field.onChange(e.target.checked)}
+                id="emailReminders"
+                className="mr-2 h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label htmlFor="emailReminders" className="text-gray-700">
+                Email Reminders
+              </label>
+            </div>
+          )}
+        />
+      </div> */}
       <div className="flex items-center justify-between">
         <button
-          type="button"
-          onClick={() => {
-            console.log(selectedDate);
-          }}
+          type="submit"
           className="bg-indigo-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"
         >
-          Schedule
+          Generate Schedule
         </button>
-        <button
+        {/* <button
           className="text-red-500 hover:underline focus:outline-none border-2 px-4 py-2 rounded-lg"
           type="button"
           onClick={() => {
@@ -58,9 +153,9 @@ const Scheduler = () => {
           }}
         >
           Cancel
-        </button>
+        </button> */}
       </div>
-    </div>
+    </form>
   );
 };
 
--- colonoready/src/routes/Root.tsx
+++ colonoready/src/routes/Root.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-
-import ColonoscopyPrepForm from "./ColonoscopyPrepForm";
-import { Outlet, useLocation } from "react-router-dom";
+import Scheduler from "../components/scheduler";
 
 const Root: React.FC = () => {
-  const location = useLocation();
-  const showForm = location.pathname === "/";
-
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold mb-8">
-        Colonoscopy Procedure Prep Forms
-      </h1>
-
-      {showForm && <ColonoscopyPrepForm />}
-      <Outlet />
+      <Scheduler />
     </div>
   );
 };
