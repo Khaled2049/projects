5f055a47cba6b80d99ad9f79e2efe6011d92d3b5
Khaled Hossain <khaledhossain.not@gmail.com>
2024-10-26 08:08:26 -0600

    minor bugs

--- novelsync/client/src/routes/Home/index.tsx
+++ novelsync/client/src/routes/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { User } from "lucide-react";
+import { Loader, User } from "lucide-react";
 
 import { useAuthContext } from "../../contexts/AuthContext";
 import { IUser } from "../../types/IUser";
@@ -13,6 +13,7 @@ const Home: React.FC = () => {
   const [following, setFollowing] = useState([] as string[]);
   const [clubs, setClubs] = useState<any>([]);
   const [loading, setLoading] = useState<string | null>(null);
+  const [usersLoading, setUsersLoading] = useState<boolean>(true);
 
   const { fetchUsersOrderedByLastLogin, user, followUser, unfollowUser } =
     useAuthContext();
@@ -20,14 +21,16 @@ const Home: React.FC = () => {
   const { getBookClubs } = useBookClub();
 
   useEffect(() => {
+    setUsersLoading(true);
     const fetchUsers = async () => {
       try {
         // Call the fetch function with the limit (e.g., 5)
         const fetchedUsers = await fetchUsersOrderedByLastLogin(5);
         const fetchedBookClubs = await getBookClubs();
-        console.log("Fetched Book Clubs", fetchedBookClubs);
         setClubs(fetchedBookClubs);
         setUsers(fetchedUsers); // Set the fetched users to state
+        setFollowing(user?.following || []); // Set the following list
+        setUsersLoading(false); // Set loading state to false
       } catch (error) {
         console.error("Error fetching users:", error);
       }
@@ -80,70 +83,69 @@ const Home: React.FC = () => {
     }
   };
 
-  // const filteredPosts =
-  //   currentTab === "myFeed"
-  //     ? posts.filter((post) => following.includes(post.author))
-  //     : posts;
-
   return (
     <div className="flex flex-col lg:flex-row min-h-screen bg-amber-50">
       {/* Left column - Authors list */}
-      <div className="w-full lg:w-1/4 bg-amber-50 p-4 overflow-y-auto">
-        <h2 className="text-2xl font-serif font-bold mb-4 text-amber-900">
-          Authors
-        </h2>
-        <ul>
-          {users.map((author) => {
-            const isFollowing = following.includes(author.uid);
-            const isLoading = loading === author.uid;
+      {usersLoading ? (
+        <Loader className="m-auto" size={48} />
+      ) : (
+        <div className="w-full lg:w-1/4 bg-amber-50 p-4 overflow-y-auto">
+          <h2 className="text-2xl font-serif font-bold mb-4 text-amber-900">
+            Authors
+          </h2>
+          <ul>
+            {users.map((author) => {
+              const isFollowing = following.includes(author.uid);
+              const isLoading = loading === author.uid;
 
-            return (
-              <li
-                key={author.uid}
-                className="flex items-center justify-between mb-2 p-2"
-              >
-                <div className="flex items-center">
-                  <User className="mr-2 text-amber-700" />
-                  <span className="font-serif text-amber-800">
-                    {author.username}
-                  </span>
-                </div>
-                {author.uid !== user?.uid && (
-                  <button
-                    onClick={() =>
-                      isFollowing
-                        ? handleUnfollow(author.uid)
-                        : handleFollow(author.uid)
-                    }
-                    className={`flex items-center ${
-                      isFollowing
-                        ? "text-red-700 bg-red-100 hover:bg-red-200"
-                        : "text-amber-700 bg-amber-100 hover:bg-amber-200"
-                    } rounded p-2 transition duration-300`}
-                    aria-label={`${isFollowing ? "Unfollow" : "Follow"} ${
-                      author.username
-                    }`}
-                    disabled={isLoading} // Disable the button while loading
-                  >
-                    {isLoading ? (
-                      <AiOutlineLoading3Quarters className="animate-spin text-lg" />
-                    ) : isFollowing ? (
-                      <>Unfollow</>
-                    ) : (
-                      <>
-                        Follow <AiOutlinePlus className="ml-1 text-lg" />
-                      </>
-                    )}
-                    <span className="sr-only">
-                      {isFollowing ? "Unfollow" : "Follow"} {author.username}
+              return (
+                <li
+                  key={author.uid}
+                  className="flex items-center justify-between mb-2 p-2"
+                >
+                  <div className="flex items-center">
+                    <User className="mr-2 text-amber-700" />
+                    <span className="font-serif text-amber-800">
+                      {author.username}
                     </span>
-                  </button>
-                )}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+                  </div>
+                  {author.uid !== user?.uid && (
+                    <button
+                      onClick={() =>
+                        isFollowing
+                          ? handleUnfollow(author.uid)
+                          : handleFollow(author.uid)
+                      }
+                      className={`flex items-center ${
+                        isFollowing
+                          ? "text-red-700 bg-red-100 hover:bg-red-200"
+                          : "text-amber-700 bg-amber-100 hover:bg-amber-200"
+                      } rounded p-2 transition duration-300`}
+                      aria-label={`${isFollowing ? "Unfollow" : "Follow"} ${
+                        author.username
+                      }`}
+                      disabled={isLoading} // Disable the button while loading
+                    >
+                      {isLoading ? (
+                        <AiOutlineLoading3Quarters className="animate-spin text-lg" />
+                      ) : isFollowing ? (
+                        <>Unfollow</>
+                      ) : (
+                        <>
+                          Follow <AiOutlinePlus className="ml-1 text-lg" />
+                        </>
+                      )}
+                      <span className="sr-only">
+                        {isFollowing ? "Unfollow" : "Follow"} {author.username}
+                      </span>
+                    </button>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
 
       <Posts />
 
